f732af7fba8bd64bb1215c017c9f23b300facbe7
choiyujin <gch04750@gmail.com>
2019-01-24 14:57:47 +0900

    good

--- BlackMirror/src/App.vue
+++ BlackMirror/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,7 +13,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #ffffff;
-  background-color: black
+  background-color: black;
+  background-size: 500px 900px;
 }
 #nav {
   padding: 30px;
--- BlackMirror/src/components/HelloWorld.vue
+++ BlackMirror/src/components/HelloWorld.vue
@@ -1,33 +1,21 @@
 <template>
   <div class="hello">
-    <button @click="getInfo">정보 받아오기 </button>
-    <table>
-      <thead>
-
-      </thead>
-      <tbody>
-        <tr v-for="(tr, i) in data" :key="i">
-          <td v-if="tr">
-            {{tr[0]}}
-          </td>
-          <td v-if="tr">
-            {{tr[1]}}
-          </td>
-          <td v-if="tr">
-            {{tr[2]}}
-          </td>
-          <td v-if="tr">
-            {{tr[3]}}
-          </td>
-          <td v-if="tr">
-            {{tr[4]}}
-          </td>
-          <td v-if="tr">
-            {{tr[5]}}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="card" style="float:left">
+      <h1>오늘의 날씨정보</h1>
+      <h1><img  src="../assets/sun.png" width="50" height="50" > {{data[0]}} ºC</h1>
+      {{data[1]}} <br/>
+      {{data[2]}} <br/>
+    </div>
+    <br/>
+    <div class="card" id="card2" style="float:left">
+      <table style="font-size : 18px; font-weight : lighter ">
+        <tr><th>{{data[3]}} :</th><th>{{data[4]}}</th></tr>
+        <tr><th>{{data[5]}} :</th><th>{{data[6]}}</th></tr>
+        <tr><th>{{data[7]}} :</th><th>{{data[8]}}</th></tr>
+      </table>
+      오늘 오전의 강수확률은 {{data[9]}} 입니다<br/>
+      오늘 오후의 강수확률은 {{data[10]}} 입니다<br/>
+    </div>
   </div>
 </template>
 
@@ -49,13 +37,23 @@ export default {
   },
   data () {
     return {
-      data: null
+      data: null,
+      weather: 0
     }
+  },
+  mounted: function() {
+    console.log('in mounted')
+    this.getInfo()
   },
   methods: {
     getInfo: function(){
       this.$socket.emit('mise')
       console.log('getMise called')
+    },
+    getImg: function(){
+
+
+
     }
   }
 }
@@ -63,6 +61,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello{
+  font-weight: 300;
+  padding-bottom: 700px;
+}
+#card2{
+  padding-top: 50px;
+  font-size: 15px;
+}
+h1 {
+  font-weight: 500;
+}
+h2{
+  font-weight: 500;
+}
 h3 {
   margin: 40px 0 0;
 }
